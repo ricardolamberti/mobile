b234b313df6359269be189f09cea1023dd19fe08
ricardolamberti <44089144+ricardolamberti@users.noreply.github.com>
2025-11-15 11:05:07 -0300

    Migrate JSON components to null safety

--- lib/json_textform/components/JSONActionBarSideBar.dart
+++ lib/json_textform/components/JSONActionBarSideBar.dart
@@ -1,44 +1,36 @@
 
-import 'package:astor_mobile/astorScreen.dart';
-import 'package:astor_mobile/model/AstorProvider.dart';
 import 'package:astor_mobile/model/astorSchema.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:provider/provider.dart';
 
-import '../../main.dart';
 import '../JSONForm.dart';
-import 'JSONDiv.dart';
-import 'JSONIcon.dart';
 import 'JSONNavigationBar.dart';
 
-typedef void OnChange(bool value);
+typedef OnChange = void Function(bool value);
 
 class JSONActionBarSideBar extends StatelessWidget {
   final AstorComponente schema;
-  final OnChange onSaved;
+  final OnChange? onSaved;
   final OnPressed onPressed;
-  final _formKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  JSONActionBarSideBar({
-    @required this.schema,
-    @required this.onPressed,
+  const JSONActionBarSideBar({
+    Key? key,
+    required this.schema,
+    required this.onPressed,
     this.onSaved,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    List<AstorComponente> schemaList = schema.components.first.options;
+    final List<AstorComponente> schemaList = schema.components.first.options;
 
-    return  Drawer(
+    return Drawer(
       key: _formKey,
       child: ListView.builder(
-
         itemBuilder: (BuildContext context, int index) =>
-            DataPopUp(schemaList[index],onPressed,context),
+            DataPopUp(schemaList[index], onPressed, context),
         itemCount: schemaList.length,
-
-      )
+      ),
     );
   }
 
--- lib/json_textform/components/JSONCard.dart
+++ lib/json_textform/components/JSONCard.dart
@@ -1,27 +1,20 @@
-
-import 'package:astor_mobile/astorScreen.dart';
 import 'package:astor_mobile/model/AstorProvider.dart';
 import 'package:astor_mobile/model/astorSchema.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
-import '../../main.dart';
 import '../JSONForm.dart';
 import 'JSONDiv.dart';
-import 'JSONIcon.dart';
-import 'LoadingDialog.dart';
-
-typedef void OnChange(bool value);
 
 class JSONCard extends StatefulWidget {
   final AstorComponente schema;
   final OnBuildBody onBuildBody;
 
-  JSONCard({
-    @required this.schema,
-    @required this.onBuildBody,
-  });
+  const JSONCard({
+    Key? key,
+    required this.schema,
+    required this.onBuildBody,
+  }) : super(key: key);
 
   @override
   _JSONCardState createState() => _JSONCardState();
@@ -32,7 +25,7 @@ class _JSONCardState extends State<JSONCard> {
   final _formKey = GlobalKey<FormState>();
 
   _JSONCardState();
-  Future<AstorApp>  futureComponente=null;
+  Future<AstorApp>? futureComponente;
 
 
   @override
@@ -44,40 +37,48 @@ class _JSONCardState extends State<JSONCard> {
   @override
   Widget build(BuildContext context) {
     if (widget.schema.diferido && widget.schema.components.isEmpty) {
-      futureComponente=onDiferidoForm(widget.schema, context);
+      futureComponente = onDiferidoForm(widget.schema, context);
     }
 
-    if (futureComponente==null)
+    if (futureComponente == null) {
       return JSONDiv(
         schema: widget.schema,
         useBootstrap: true,
         actionBar: false,
         onBuildBody: widget.onBuildBody,
       );
+    }
     return FutureBuilder<AstorApp>(
-        future: futureComponente,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return JSONDiv(
-              schema: widget.schema,
-              useBootstrap: true,
-              actionBar: false,
-              onBuildBody: widget.onBuildBody,
-            );;
-          } else if (snapshot.hasError) {
-            return Text('error');
-          }
-          return  CircularProgressIndicator();
-        });
+      future: futureComponente,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return JSONDiv(
+            schema: widget.schema,
+            useBootstrap: true,
+            actionBar: false,
+            onBuildBody: widget.onBuildBody,
+          );
+        } else if (snapshot.hasError) {
+          return const Text('error');
+        }
+        return const CircularProgressIndicator();
+      },
+    );
 
 
   }
 
 
-  Future<AstorApp> onDiferidoForm(AstorComponente schema, [BuildContext context]) async {
-
-      AstorProvider astorProvider = Provider.of(context, listen: false);
-      return astorProvider.doDiferido(schema, context, schema.actionTarget);
+  Future<AstorApp> onDiferidoForm(AstorComponente schema,
+      [BuildContext? customContext]) async {
+    final BuildContext effectiveContext = customContext ?? context;
+    final AstorProvider astorProvider =
+        Provider.of<AstorProvider>(effectiveContext, listen: false);
+    return astorProvider.doDiferido(
+      schema,
+      effectiveContext,
+      schema.actionTarget,
+    );
   }
 }
 
